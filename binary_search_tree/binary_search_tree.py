617f0708eb3b95971a6186eba0bb8f60dc28db81
Barbara Mayfield <bmayfield1991@gmail.com>
2020-06-23 17:03:22 -0500

    work: Part 1 of binary search trees finished

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -20,27 +20,29 @@ class BSTNode:
     # Insert the given value into the tree
     def insert(self, value):
         # take the current value of our node (self.value)
-        current_value = self.value
-
+        if self.value is None:
+            return None
         # compare to the new value we want to insert
-        if value < current_value:
-            # IF self.left is already taken by a node,
-            if self.left:
-                # make that (left) node call insert
-                new_value = self.left.insert(value)
+        if self.value > value:
+            # If self.left isn't already taken by a node,
+            if self.left is None:
                 # set the left child to the new node with the new value
-                return new_value
+                self.left = BSTNode(value)
+            else:
+                # make that (left) node call insert
+                self.left.insert(value)
 
-        elif value >= self.value:
-            # IF self.right is already taken by a node,
-            if self.right:
-                # make that (right) node call insert
-                new_value = self.right.insert(value)
-        # set the right child to the new node with new value
-                return new_value
-
+        else:
+            # If self.right isn't already taken by a node,
+            if self.right is None:
+                # set the right child to the new node with new value
+                self.right = BSTNode(value)
+            else:
+                # otherwise, make that (right) node call insert
+                self.right.insert(value)
     # Return True if the tree contains the value
     # False if it does not
+
     def contains(self, target):
         if self.value == target:
             return True
@@ -68,23 +70,23 @@ class BSTNode:
     def get_max(self):
         # the largest value will always be to the right of the current node
         # if we can go right, lets find the largest number there by calling get_max on the right subtree
-        # if we cannot go right, return the current value
-        max_val = self.right.value
-
-        if self.value > max_val:
-            max_val = self.value
-        return max_val
+        if self.right:
+            return self.right.get_max()
+        else:
+            # if we cannot go right, return the current value
+            return self.value
 
     # Call the function `fn` on the value of each node
+
     def for_each(self, fn):
         # call function on the current value fn(self.value)
         fn(self.value)
         # if you can go left, call for_each on the left tree
-        if self.left is not None:
-            self.right.for_each(fn)
-        # if you can go right, call for_each on the right tree
-        if self.right is not None:
+        if self.left:
             self.left.for_each(fn)
+        # if you can go right, call for_each on the right tree
+        if self.right:
+            self.right.for_each(fn)
 
     # Part 2 -----------------------
 
